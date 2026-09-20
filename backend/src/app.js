c0b77d91242825c61486c5cdf414b7a478cd8479
mohmrizwan <151992791+mohmrizwan@users.noreply.github.com>
2026-09-20 23:20:09 +0530

    Added cloudinary

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,9 @@ import express from "express";
 import cors from "cors";
 
 import AuthUser from "./Routes/Users/AuthRoutes.js";
-import profileRoutes from "../src/Routes//Users/profileRoutes.js";
-import VendorRoutes from "../src/Routes/Vendor/AuthRoutes.js";
-import ProductsRoutes from "../src/Routes/Vendor/ProductsRoutes.js";
+import profileRoutes from "./Routes/Users/profileRoutes.js";
+import VendorRoutes from "./Routes/Vendor/AuthRoutes.js";
+import ProductsRoutes from "./Routes/Vendor/ProductsRoutes.js";
 
 export default function createApp() {
   const app = express();
